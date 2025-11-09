f9fd2a354d728d5d8027342be6760b73337b3cff
sany13-94 <sanaalkrn@gmail.com>
2025-11-09 17:41:35 +0000

    update

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -7,4 +7,4 @@ from .federated_emnist_iid import FederatedEMNISTDatasetIID
 from .federated_emnist_noniid import FederatedEMNISTDataset_nonIID
 
 __all__ = ['FederatedEMNISTDataset', 'FederatedEMNISTDatasetIID', 'FederatedEMNISTDataset_nonIID',
-            'FederatedCIFAR100Dataset', 'PartitionedCIFAR10Dataset', 'RedditDataset', 'CelebADataset']
+            'FederatedCIFAR100Dataset', 'PartitionedCIFAR10Dataset', 'RedditDataset', 'PathMNISTDomain']
