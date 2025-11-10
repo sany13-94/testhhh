6ac25b7273c03a386e79406a123f6a71804ab77a
sany13-94 <sanaalkrn@gmail.com>
2025-11-10 01:10:47 +0000

    update

--- src/FL_core/trainer.py
+++ src/FL_core/trainer.py
@@ -100,7 +100,7 @@ class Trainer:
             for num_update, (input, labels) in enumerate(dataloader):
                 input, labels = input.to(self.device), labels.to(self.device)
                 print(labels.shape)
-
+                print(input.shape)
                 optimizer.zero_grad()
                 output = self.model(input)
                 _, preds = torch.max(output.detach().data, 1)
